5c1eae030ce1add336f3fea6a55cbb832634f01b
Rajan66 <rajanmaharjan042@gmail.com>
2025-02-23 01:49:59 +0545

    feat: add Trending section

--- src/components/home/Trending.tsx
+++ src/components/home/Trending.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+
 import Button from "../common/Button";
+import TrendingCard from "./TrendingCard";
+
+import tees from "@/assets/tees.jpg";
+import shirts from "@/assets/shirts.jpg";
+import jorts from "@/assets/jorts.jpg";
+import shorts from "@/assets/shorts.jpg";
 
 const Trending = () => {
     return (
-        <div className="p-4">
+        <div className="section-margin">
             <div className="flex justify-between">
                 <h1 className="uppercase text-3xl font-extrabold tracking-wide">
                     Trending Categories
@@ -14,16 +21,22 @@ const Trending = () => {
                         size="xl"
                         className="text-base rounded-none uppercase"
                     >
-                        Womens
+                        Mens
                     </Button>
                     <Button
                         variant="primary"
                         size="xl"
                         className="text-base border border-secondary rounded-none uppercase"
                     >
-                        Mens
+                        Womens
                     </Button>
                 </div>
+            </div>
+            <div className="grid grid-cols-2 xl:grid-cols-4 mt-4 gap-6">
+                <TrendingCard title="tees" image={tees} />
+                <TrendingCard title="jorts" image={shirts} />
+                <TrendingCard title="shirts" image={jorts} />
+                <TrendingCard title="shorts" image={shorts} />
             </div>
         </div>
     );
---
+++ src/components/home/TrendingCard.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+
+import { Card, CardDescription, CardHeader } from "../common/Card";
+
+type TrendingCardProps = {
+    image: string | StaticImport;
+    title: string;
+};
+const TrendingCard = ({ image, title }: TrendingCardProps) => {
+    return (
+        <Link href={"/shop"} className="flex">
+            <Card className="w-[430px] h-[600px]">
+                <CardHeader className="relative p-0 h-full">
+                    <Image
+                        src={image}
+                        width={400}
+                        height={600}
+                        alt="model"
+                        className="w-full h-full object-cover"
+                    />
+                    <CardDescription className="absolute bottom-4 flex flex-col items-start text-white px-4 gap-2">
+                        <h2 className="text-4xl font-bold uppercase">{title}</h2>
+                        <p className="uppercase tracking-wide">Shop now</p>
+                    </CardDescription>
+                </CardHeader>
+            </Card>
+        </Link>
+    );
+};
+
+export default TrendingCard;
