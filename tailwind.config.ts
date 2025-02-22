196dd527c80748d3f6a7cc04739108a4bd821dfb
Rajan66 <rajanmaharjan042@gmail.com>
2025-02-22 13:23:37 +0545

    feat: add font and screen sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,30 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                spectral: "var(--font-spectral)",
+            },
+            screens: {
+                xs: "480px",
+                sm: "640px",
+                md: "768px",
+                lg: "1024px",
+                xl: "1280px",
+                "2xl": "1536px",
+                "3xl": "1600px",
+            },
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
