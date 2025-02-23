0b8a94ed3c50b83ee621240ba32aee65e2d1d36c
Rajan66 <rajanmaharjan042@gmail.com>
2025-02-23 12:12:20 +0545

    refactor: move client side to separate component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,12 @@
-// TODO bad to make the whole page client side
-"use client"
-import { useGetProducts } from "@/hooks/productQueries";
 import Hero from "@/components/home/Hero";
 import Trending from "@/components/home/Trending";
 import SponsorCard from "@/components/home/SponsorCard";
-import ProductCard from "@/components/home/ProductCard";
+import ProductList from "@/components/home/ProductList";
+
 import billie from "@/assets/billie.jpg";
+import party from "@/assets/party.jpg";
 
 const page = () => {
-    const { data, isLoading, error } = useGetProducts();
-    console.log(data);
-
     return (
         <div className="">
             <Hero
@@ -28,23 +24,22 @@ const page = () => {
                 secondBtn={false}
                 flag={true}
             />
-            <SponsorCard
-                title="Graphic Tees"
-                image={billie}
-                description="Billie Ellish Oversized Tee $70"
-                buttonTitle="Shop Womens"
-            />
-            <div className="section-margin flex gap-6">
-                {Array.from({ length: 4 }, (_, i) => (
-                    <ProductCard
-                        key={i}
-                        title="Luck & Trouble"
-                        image={billie}
-                        description="Frankie cap sleeve mini dress brown"
-                        price={90.0}
-                    />
-                ))}
+
+            <div className="grid grid-cols-1 lg:grid-cols-2 section-margin gap-4">
+                <SponsorCard
+                    title="Graphic Tees"
+                    image={billie}
+                    description="Billie Ellish Oversized Tee $70"
+                    buttonTitle="Shop Womens"
+                />
+                <SponsorCard
+                    title="Party Shirts"
+                    image={party}
+                    description="Neovision on Lock Shirt $70"
+                    buttonTitle="Shop Mens"
+                />
             </div>
+            <ProductList />
         </div>
     );
 };
