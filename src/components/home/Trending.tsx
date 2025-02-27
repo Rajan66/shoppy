fb2fc35932144136f9a97aac708363350f65eff4
Rajan66 <rajanmaharjan042@gmail.com>
2025-02-27 23:06:28 +0545

    feat: filter trending based on gender

--- src/components/home/Trending.tsx
+++ src/components/home/Trending.tsx
@@ -1,64 +1,77 @@
 "use client";
 import { Category } from "@/types";
 import { useGetCategories } from "@/hooks/categoryQueries";
+import { useEffect, useState } from "react";
 
 import Button from "../common/Button";
 import TrendingCard from "./TrendingCard";
 import TrendingCardSkeleton from "./skeleton/TrendingCardSkeleton";
+import TrendingCategory from "./TrendingCategory";
 
 const Trending = () => {
+    const [genderFilter, setGenderFilter] = useState<"Mens" | "Womens">("Mens");
     const { data: categories, isLoading, error } = useGetCategories();
+    const [filteredCategories, setFilteredCategories] = useState<Category[]>([]);
 
+    useEffect(() => {
+        const filtered = categories?.filter(
+            (category: Category) => category?.gender === genderFilter
+        );
+        setFilteredCategories(filtered || []);
+    }, [categories, genderFilter]);
+
+    useEffect(() => {
+        console.log(filteredCategories);
+    }, [filteredCategories]);
     if (isLoading) {
         return (
-            <div className="section-margin grid grid-cols-4 gap-6">
-                {Array.from({ length: 4 }).map((_, index) => (
-                    <TrendingCardSkeleton key={index} />
-                ))}
+            <div className="mx-4 my-6 md:section-margin">
+                <TrendingCategory
+                    genderFilter={genderFilter}
+                    setGenderFilter={setGenderFilter}
+                />
+
+                <div className="grid grid-cols-1 xs:grid-cols-2 xl:grid-cols-4 mt-4 gap-6">
+                    {Array.from({ length: 4 }).map((_, index) => (
+                        <TrendingCardSkeleton key={index} />
+                    ))}
+                </div>
             </div>
         );
     }
 
     if (error) {
         return (
-            <div className="section-margin grid grid-cols-4 gap-6">
-                {Array.from({ length: 4 }).map((_, index) => (
-                    <TrendingCard key={index} />
-                ))}
+            <div className="mx-4 my-6 md:section-margin">
+                <TrendingCategory
+                    genderFilter={genderFilter}
+                    setGenderFilter={setGenderFilter}
+                />
+                <div className="grid grid-cols-1 xs:grid-cols-2 xl:grid-cols-4 mt-4 gap-6">
+                    {Array.from({ length: 4 }).map((_, index) => (
+                        <TrendingCard key={index} />
+                    ))}
+                </div>
             </div>
         );
     }
+
     return (
-        <div className="section-margin">
-            <div className="flex justify-between">
-                <h1 className="uppercase text-3xl font-extrabold tracking-wide">
-                    Trending Categories
-                </h1>
-                <div className="flex gap-6">
-                    <Button
-                        variant="secondary"
-                        size="xl"
-                        className="text-base rounded-none uppercase"
-                    >
-                        Mens
-                    </Button>
-                    <Button
-                        variant="primary"
-                        size="xl"
-                        className="text-base border border-secondary rounded-none uppercase"
-                    >
-                        Womens
-                    </Button>
-                </div>
-            </div>
-            <div className="grid grid-cols-2 xl:grid-cols-4 mt-4 gap-6">
-                {categories?.items?.map((category: Category, index: number) => (
-                    <TrendingCard
-                        key={index}
-                        title={category.title}
-                        imageUrl={category.imageUrl}
-                    />
-                ))}
+        <div className="mx-4 my-6 md:section-margin">
+            <TrendingCategory
+                genderFilter={genderFilter}
+                setGenderFilter={setGenderFilter}
+            />
+            <div className="grid grid-cols-1 xs:grid-cols-2 xl:grid-cols-4 mt-4 gap-6">
+                {filteredCategories
+                    ?.slice(0, 4)
+                    .map((category: Category, index: number) => (
+                        <TrendingCard
+                            key={index}
+                            title={category.title}
+                            imageUrl={category.imageUrl}
+                        />
+                    ))}
             </div>
         </div>
     );
---
+++ src/components/home/TrendingCategory.tsx
@@ -0,0 +1,37 @@
+import React, { Dispatch, SetStateAction } from "react";
+import Button from "../common/Button";
+
+type TrendingCategoryProps = {
+    genderFilter: "Mens" | "Womens";
+    setGenderFilter: Dispatch<SetStateAction<"Mens" | "Womens">>;
+};
+
+const TrendingCategory = ({ setGenderFilter, genderFilter }: TrendingCategoryProps) => {
+    return (
+        <div className="flex flex-col gap-4 md:flex-row justify-between">
+            <h1 className="uppercase text-3xl font-extrabold tracking-wide">
+                Trending Categories
+            </h1>
+            <div className="flex gap-6">
+                <Button
+                    variant={genderFilter === "Mens" ? "secondary" : "primary"}
+                    size="xl"
+                    className="text-base rounded-none uppercase"
+                    onClick={() => setGenderFilter("Mens")}
+                >
+                    Mens
+                </Button>
+                <Button
+                    variant={genderFilter === "Womens" ? "secondary" : "primary"}
+                    size="xl"
+                    className="text-base border border-secondary rounded-none uppercase"
+                    onClick={() => setGenderFilter("Womens")}
+                >
+                    Womens
+                </Button>
+            </div>
+        </div>
+    );
+};
+
+export default TrendingCategory;
